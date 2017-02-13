d6e03905f6cf772ef147d4d8f04d51ae51f12ec4
Shyam <s4shyam95@gmail.com>
2017-02-13 09:49:06 +0530

    proc

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn flockProj2.wsgi --log-file -
+
---
+++ flockProj2/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for flockProj2 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flockProj2.settings")
+from dj_static import Cling
+application = Cling(get_wsgi_application())
