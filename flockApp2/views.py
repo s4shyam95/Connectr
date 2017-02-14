f116bfca12e80fd294360030e2833062d08bda95
Shyam <s4shyam95@gmail.com>
2017-02-14 16:59:54 +0530

    test12

--- flockApp2/views.py
+++ flockApp2/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+from django.utils.html import escape
 import json
 @csrf_exempt
 def listen(request):
-    return HttpResponse(str(request))
+    print escape(repr(request))
+    return HttpResponse('listen')
 
 @csrf_exempt
 def configure(request):
-    return HttpResponse(str(request))
+    print escape(repr(request))
+    return HttpResponse('configured')
 
 
 @csrf_exempt
