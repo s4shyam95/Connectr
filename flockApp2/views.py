2d8d731c4841b881d58f603c2f42072d2f204895
Shyam <s4shyam95@gmail.com>
2017-02-18 01:04:32 +0530

    t4

--- flockApp2/views.py
+++ flockApp2/views.py
@@ -188,6 +188,9 @@ default_client = "Shyam"
 @csrf_exempt
 def voice(request):
     dest_number = request.POST['PhoneNumber']
+    ll = Log()
+    ll.text = str(request.POST['PhoneNumber'])
+    ll.save(force_insert=True)
     resp = twilio.twiml.Response()
     with resp.dial(callerId=caller_id) as r:
         if dest_number and re.search('^[\d\(\)\- \+]+$', dest_number):
