3f7049b4694b9306fab056a74050809b2b1b6a85
Shyam <s4shyam95@gmail.com>
2017-02-18 18:55:11 +0530

    try again

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn flockProj2.wsgi --log-file=-
+web: python manage.py makemigrations;gunicorn -b 0.0.0.0:\$PORT -w 3 --env DJANGO_SETTINGS_MODULE=flockProj2.settings flockProj2.wsgi
 
--- flockProj2/settings.py
+++ flockProj2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -60,14 +60,18 @@ WSGI_APPLICATION = 'flockProj2.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = {}
+if os.environ.get('DATABASE_URL') is None:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
