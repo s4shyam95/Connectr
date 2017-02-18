e79b30389035e52cb60c9c49f053625656774105
Shyam <s4shyam95@gmail.com>
2017-02-18 17:36:02 +0530

    try again

--- flockApp2/views.py
+++ flockApp2/views.py
@@ -360,6 +360,7 @@ def save_interactions(request):
         c = c[0]
     c.access_token = access_token
     c.name = company_name
+    c.number = TWILIO_DEFAULT_CALLERID
     c.save()
     interactions = request.POST['interactions']
     interactions = json.loads(interactions)
