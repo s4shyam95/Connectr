f9b858161ad94610f761b3cc0250004eda95286e
Shyam <s4shyam95@gmail.com>
2017-02-18 17:54:36 +0530

    try again

--- flockProj2/settings.py
+++ flockProj2/settings.py
@@ -55,21 +55,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'flockProj2.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
 
 WSGI_APPLICATION = 'flockProj2.wsgi.application'
 
